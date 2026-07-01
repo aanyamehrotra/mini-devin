ce2eb17735bea048a6e843a8d78e1a459ce15895
aanyamehrotra <3103aanya@gmail.com>
2026-07-01 19:05:12 +0530

    Build initial FastAPI backend with Gemini integration

--- main.py
+++ main.py
@@ -1,8 +1,28 @@
-from fastapi import FastAPI, HTTPException
-from fastapi.middleware.cors import CORSMiddleware
+from fastapi import FastAPI
 from pydantic import BaseModel
-from anthropic import Anthropic
+from google import genai
 import os
 from dotenv import load_dotenv
+
 load_dotenv()
 app=FastAPI()
+client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
+
+class ChatRequest(BaseModel):
+    prompt:str 
+
+@app.get('/')
+def root():
+    return {
+        "message":'Mini-Devin is running'
+    }
+
+@app.post('/chat')
+def chat(request:ChatRequest):
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=request.prompt
+    )
+    return {
+        'response': response.text
+    }
